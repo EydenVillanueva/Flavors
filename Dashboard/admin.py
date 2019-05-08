5e526370a4cc2eeb89d2c18c6692be07119f6719
Erick <cruzperick@gmail.com>
2019-05-08 12:37:44 -0500

    Realizado los modelos en base al diagrama.

--- Dashboard/admin.py
+++ Dashboard/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Client, Category, Dish, Flavor, Restaurant, Shedule
 
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Category)
+admin.site.register(Dish)
+admin.site.register(Flavor)
+admin.site.register(Restaurant)
+admin.site.register(Shedule)
--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Timestamp(models.Model):
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+    delete_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.IntegerField()
+    plan = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.user
+
+class Restaurant(Timestamp):
+    name = models.CharField( max_length=50)
+    address = models.TextField()
+    logo_url = models.URLField(max_length=200)
+    web_site = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+
+class Dish(models.Model):
+    name = models.CharField( max_length=50)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    categories = models.ManyToManyField('Category')
+    flavors = models.ManyToManyField('Flavor')
+
+    def __str__(self):
+        return self.name
+
+class Category(Timestamp):
+    name = models.CharField( max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Flavor(Timestamp):
+    name = models.CharField( max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Shedule(models.Model):
+    days = models.TextField()
+    time_open = models.DateTimeField()
+    time_close = models.DateTimeField()
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.restaurant.name
