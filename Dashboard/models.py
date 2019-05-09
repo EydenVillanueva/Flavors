717420f4a9f3266aba24e2fcccb0cc93eb32f242
Henry David Chi Azcorra <1620270@uqroo.mx>
2019-05-09 13:11:06 -0500

    Modificacion de modelo Restaurant

--- Dashboard/models.py
+++ Dashboard/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-#Timestamp model to save the date of every transaction of the rest of the models
+
+# Timestamp model to save the date of every transaction of the rest of the models
 class Timestamp(models.Model):
     create_at = models.DateField(auto_now_add=True)
     update_at = models.DateField(auto_now=True)
@@ -9,6 +10,7 @@ class Timestamp(models.Model):
 
     class Meta:
         abstract = True
+
 
 # Client model that will be used to register new clients in the system
 class Client(models.Model):
@@ -18,21 +20,24 @@ class Client(models.Model):
     city = models.CharField(max_length=50)
 
     def __str__(self):
-        return self.user
+        return self.user.username
 
-#Restaurant model to every restaurant saved in the system.
+
+# Restaurant model to every restaurant saved in the system.
 class Restaurant(Timestamp):
-    name = models.CharField( max_length=50)
+    name = models.CharField(max_length=50)
     address = models.TextField()
     logo_url = models.URLField(max_length=200)
     web_site = models.URLField(max_length=200)
+    status = models.BooleanField('open', blank=False, null=False, default=True)
+    owner = models.ForeignKey(Client, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-    
+
 
 class Dish(models.Model):
-    name = models.CharField( max_length=50)
+    name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
@@ -42,15 +47,16 @@ class Dish(models.Model):
     def __str__(self):
         return self.name
 
+
 class Category(Timestamp):
-    name = models.CharField( max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
 
 
 class Flavor(Timestamp):
-    name = models.CharField( max_length=50)
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
