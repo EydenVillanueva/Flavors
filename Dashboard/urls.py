97eef13a4009df99e3c1f68842cc28d14930ed5d
Erick <cruzperick@gmail.com>
2019-05-15 00:14:13 -0500

    Cambiado la funcion de logueo y  home a clases.

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'Dashboard'
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('login/', views.userlogin, name="login"),
-     path('register/', views.new_user, name="new_user"),
+    path('', views.Home.as_view(), name='home'),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('register/', views.new_user, name="new_user"),
     #path('register/', views.CreateView.as_view(),name="new_user"),
 ]
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -4,23 +4,29 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect, reverse
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+from django.http.response import HttpResponseRedirect
+from django.urls import reverse_lazy
 from .forms import LoginForm, UserForm, ClientForm
-from django.views.generic import CreateView
+from django.views.generic import CreateView, FormView, TemplateView
 from .models import Client
+
 
 # Create your views here.
 def new_user(request):
     error = False
-    
+
     if request.method == 'POST':
         user_form = UserForm(request.POST)
         client_form = ClientForm(request.POST)
 
         if user_form.is_valid() and client_form.is_valid():
-            #E-mail check
-            repeat_email = User.objects.filter(email=user_form.cleaned_data["email"])
-            #Username check
-            repeat_user = User.objects.filter(username=user_form.cleaned_data["username"])
+            # E-mail check
+            repeat_email = User.objects.filter(
+                email=user_form.cleaned_data["email"])
+            # Username check
+            repeat_user = User.objects.filter(
+                username=user_form.cleaned_data["username"])
 
             if not repeat_email.exists():
                 user = user_form.save()
@@ -29,39 +35,40 @@ def new_user(request):
                 client.user = user
 
                 client_form.save()
-                
+
                 username = user_form.cleaned_data.get('username')
                 password = user_form.cleaned_data.get('password')
                 user = authenticate(username=username, password=password)
                 login(request, user)
 
-                return redirect('/')            
+                return redirect('/')
     else:
         user_form = UserForm()
         client_form = ClientForm()
 
-    context = {'user_form':user_form, 'client_form':client_form}
-    
+    context = {'user_form': user_form, 'client_form': client_form}
+
     return render(request, 'Dashboard/new-user-form.html', context)
 
-def userlogin(request):
-    if request.method == 'POST':
-    
-        user = authenticate(
-            username=request.POST['username'],
-            password=request.POST['password']
-        )
-        if user is not None:
-            login(request, user)
-            return redirect('Dashboard:home')
-        else:
-            login_form = LoginForm()
-            error = True
-            return render(request, 'Dashboard/login.html', {'login_form': login_form, 'error': error})
-    else:
-        login_form = LoginForm()
-        error = False
-        return render(request, 'Dashboard/login.html', {'login_form': login_form, 'error': error})
+class LoginView(FormView):
+    form_class = AuthenticationForm
+    template_name = 'Dashboard/login.html'
+    success_url = reverse_lazy("Dashboard:home")
 
-def home(request):
-    return render(request, 'Dashboard/index.html')
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return super(LoginView, self).dispatch(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        if form.is_valid():
+            login(self.request, form.get_user())
+            return super(LoginView, self).form_valid(form)
+        else:
+            return super(LoginView, self).form_invalid(form)
+
+
+class Home(TemplateView):
+    template_name = "Dashboard/index.html"
